ba196625eb07a32a341b63bdb28e77bd18ff8472
Ali Aidid <aliaidid@gmail.com>
2019-05-16 15:22:21 +0700

    function Navbar Active

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,15 @@
 /*==================================================================================
     Custom JS (Any custom js code you want to apply should be defined here).
 ====================================================================================*/
+var page = window.location.pathname;
+if (page == "/about") {
+  $(".aboutCss").addClass("active");
+} else if (page == "/") {
+  $(".homeCss").addClass("active");
+} else if (page == "/mobile" || page == "/website") {
+  $(".programsCss").addClass("active");
+} else if (page == "/faq") {
+  $(".faqCss").addClass("active");
+} else if (page == "/contact") {
+  $(".contactCss").addClass("active");
+}
