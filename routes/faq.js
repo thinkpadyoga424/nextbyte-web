084af85a437bfa748d5e41e1838b3ef6e7d6ae33
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-05-16 16:58:24 +0700

    implement fetching data from database for FAQ page

--- routes/faq.js
+++ routes/faq.js
@@ -10,7 +10,8 @@ router.get("/", function(req, res, next) {
     .get("https://us-central1-dbnextbyte.cloudfunctions.net/api/v1/faqs")
     .then(response => {
       data.push(response.data.data);
-      console.log(data[0].length);
+      console.log(data[0][1].type);
+      console.log(data[0][1].answer);
       res.render("faq", { data: data });
     })
     .catch(error => {
