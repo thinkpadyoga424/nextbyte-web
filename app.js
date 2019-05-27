57b86978f80e9c6d1b93cd5c2992fb844c707a25
Panji Gumelar <panjiggm@gmail.com>
2019-05-27 18:11:54 +0700

    just commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -6,15 +5,6 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const sm = require('sitemap')
-=======
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var cors = require("cors");
-const sm = require("sitemap");
->>>>>>> dc2b452db8ae06fd47f404de3400e37fdd1fd874
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
