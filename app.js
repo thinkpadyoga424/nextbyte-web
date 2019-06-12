7d10d97976e241ef4eb58211c6ffc616f54f3487
Panji Gumelar <panjiggm@gmail.com>
2019-06-12 10:47:41 +0700

    ncommenting appjs

--- app.js
+++ app.js
@@ -24,17 +24,17 @@ const app = express();
 app.use(cors());
 
 // app.use(redirectHttps([/localhost:(\d{4})/], [/\/error/], 301));
-// app.use(function requireHTTPS(req, res, next) {
+app.use(function requireHTTPS(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
-//   if (
-//     !req.secure &&
-//     req.get("x-forwarded-proto") !== "https" &&
-//     process.env.NODE_ENV !== "development"
-//   ) {
-//     return res.redirect("https://" + req.get("host") + req.url);
-//   }
-//   next();
-// });
+  if (
+    !req.secure &&
+    req.get("x-forwarded-proto") !== "https" &&
+    process.env.NODE_ENV !== "development"
+  ) {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+});
 
 // Robot.txt
 app.use("/robots.txt", function(req, res, next) {
