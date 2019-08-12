fc9f108f5500c1f541ccf3cac211f85c2aca2195
Panji Gumelar <panjiggm@gmail.com>
2019-08-12 15:52:52 +0700

    create graduate page

--- routes/graduate.js
+++ routes/graduate.js
@@ -1,8 +1,11 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const Axios = require('axios')
 
 /* GET Graduate page. */
 router.get("/", function(req, res, next) {
+
   res.render("graduate", { title: "Lulusan Kami" });
 });
 
